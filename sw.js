6e53e35e783751ac071afa171185f36b73af8ab0
alamsatriasince2023-coder <alamsatria.since2023@gmail.com>
2026-06-26 04:49:03 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -59,3 +59,43 @@ self.addEventListener(
     }
 
 );
+
+self.addEventListener(
+
+'fetch',
+
+event=>{
+
+event.respondWith(
+
+fetch(event.request)
+
+.catch(
+
+()=>{
+
+return caches.match(
+
+event.request
+
+)
+
+.then(response=>{
+
+return response ||
+
+caches.match(
+
+'/offline.html'
+
+);
+
+});
+
+}
+
+)
+
+);
+
+});
