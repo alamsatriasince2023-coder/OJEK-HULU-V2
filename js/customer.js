aa31b3e5a3d0732f371fc9c8585b66615efbe05e
alamsatriasince2023-coder <alamsatria.since2023@gmail.com>
2026-06-25 15:04:29 +0700

    Update customer.js

--- js/customer.js
+++ js/customer.js
@@ -1,1 +1,47 @@
+import {
+    getCurrentUser,
+    logoutUser
+} from './auth.js';
 
+const user = await getCurrentUser();
+
+if (!user) {
+    location.href = 'login.html';
+}
+
+document.getElementById('welcome').textContent =
+`Halo, ${user.email}`;
+
+document
+.getElementById('btn-order')
+.addEventListener('click', () => {
+
+    location.href = 'order.html';
+
+});
+
+document
+.getElementById('btn-history')
+.addEventListener('click', () => {
+
+    location.href = 'customer-history.html';
+
+});
+
+document
+.getElementById('btn-profile')
+.addEventListener('click', () => {
+
+    alert('Profil V2 segera dibuat');
+
+});
+
+document
+.getElementById('btn-logout')
+.addEventListener('click', async () => {
+
+    await logoutUser();
+
+    location.href = 'login.html';
+
+});
